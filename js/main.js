68830bdda81b2e09ebcefe65482a3fa2db0feb16
Vlad Ungurean <136657499+VladUngurean@users.noreply.github.com>
2023-12-24 22:24:47 +0200

    dropdown logic end, styling start

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function dropDownMain() {
-  document.getElementById("myDropdown").classList.toggle("show");
+//when click on menu button it toggles 'show' and check if second dropdown is actvie then it close it
+function navDropDownMain() {
+  document.getElementById("dropdownMain").classList.toggle("show");
+  let secondDropdown = document.getElementById("dropdownSecond");
+  if (secondDropdown.classList.contains("show")) {
+    secondDropdown.classList.remove("show");
+  }
 }
 
-function dropDownSec() {
-  document.getElementById("mySecondDropdown").classList.toggle("show");
+//when click on nested menu button it toggles 'show'
+function navDropDownSecond() {
+  document.getElementById("dropdownSecond").classList.toggle("show");
 }
-
-// Close the dropdown if the user clicks outside of it
-
+// stop event propagation on nested menu button so it does not trigger main one
 document
   .getElementById("secondDropdownBtn")
   .addEventListener("click", function (event) {
     event.stopPropagation();
-    console.log("event");
   });
 
+// Close the dropdown if the user clicks outside of it
 window.addEventListener("click", function (event) {
-  if (!event.target.matches(".dropbtn", ".dropbtnSecond")) {
-    let dropdowns = document.querySelectorAll(
-      ".dropdown-content,.dropdown-content-rent"
-    );
-    console.log(dropdowns.length);
+  if (!event.target.matches(".dropbtnMain", ".dropbtnSecond")) {
+    let dropdowns = document.querySelectorAll("#dropdownMain,#dropdownSecond");
     for (i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
-      console.log(openDropdown);
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
