510c8255629a4cbb384d0b3b9fc562d48fc39fe6
Vlad Ungurean <136657499+VladUngurean@users.noreply.github.com>
2024-01-03 21:16:10 +0200

    for deploy

--- api/config.php
+++ api/config.php
@@ -1,11 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "carsrent";
 
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "carsrent";
+
+$dbhost ="sql.freedb.tech";
+$dbuser = "Vladd";
+$dbpass="1122";
+$dbname ="freedb_carsrent";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+//$conn = new mysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
