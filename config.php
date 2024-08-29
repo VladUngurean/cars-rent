7339ebe6befe9929006f3ff715060ea3f83691b6
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-08-29 18:32:58 +0300

    code cleanup

--- config.php
+++ config.php
@@ -1,27 +1,17 @@
 <?php
   // $dbhost ="sql310.infinityfree.com";
-
   // $dbuser = "if0_36986938";
-
   // $dbpass="53S9Q6NyOIa1Dhr";
-
   // $dbname ="if0_36986938_dream_cars";
 
   $dbhost ="localhost";
-
   $dbuser = "root";
-
   $dbpass="Admin112233";
-
   $dbname ="ch_dream_cars";
+
   try {
-
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
-
   } catch (mysqli_sql_exception) {
-
     echo "Can not connect to Data Base";
-
   }
-
 ?>
--- pages/adminProfile.php
+++ pages/adminProfile.php
@@ -1,71 +1,70 @@
-<?php 
-    session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-
-    <?php
-    include "ProcGetManagersAndCouriers.php";
-
-    //delete images in cars from DB
-    if(isset($_POST["deleteUser"])) {  
-
-        $emailToDelete = mysqli_real_escape_string($conn, $_POST["deleteUser"]);  
-
-        //delete cars from db
-        $stmt = $conn->prepare("CALL deleteManagerORCourier(?)");
-        
-        if ($stmt->execute([$emailToDelete])) {
-            echo '<script>alert("User successfully deleted from DB")</script>'; 
-            echo '<script> window.location.href = "adminProfile.php";</script>';
-        } else {
-            echo '<script>alert("Error deleting car: ' . $stmt->error . '")</script>'; 
-        }
-
-        // Close the statement
-        $stmt->close();
-    }
-?>
-
-    <script type='text/javascript' src="/js/admin.js" defer></script>
-</head>
-
-<body>
-    <style>
-    .register-area__input-field>p {
-        display: none;
-    }
-    </style>
-
-    <?php include "register.php";?>
-
-    <form action="" method="post">
-
-        <div class="table-container">
-            <h2>Lista de manageri si Curieri</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Role</th>
-                        <th>Nume</th>
-                        <th>Prenume</th>
-                        <th>Nr. telefon</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody id="managesAndCouriersInfoTable">
-                </tbody>
-            </table>
-        </div>
-
-    </form>
-
-</body>
-
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+
+    <?php
+    include "ProcGetManagersAndCouriers.php";
+
+    //delete images in cars from DB
+    if(isset($_POST["deleteUser"])) {  
+
+        $emailToDelete = mysqli_real_escape_string($conn, $_POST["deleteUser"]);  
+
+        //delete cars from db
+        $stmt = $conn->prepare("CALL deleteManagerORCourier(?)");
+        if ($stmt->execute([$emailToDelete])) {
+            echo '<script>alert("User successfully deleted from DB")</script>'; 
+            echo '<script> window.location.href = "adminProfile.php";</script>';
+        } else {
+            echo '<script>alert("Error deleting car: ' . $stmt->error . '")</script>'; 
+        }
+
+        // Close the statement
+        $stmt->close();
+    }
+?>
+
+    <script type='text/javascript' src="/js/admin.js" defer></script>
+</head>
+
+<body>
+    <style>
+    .register-area__input-field>p {
+        display: none;
+    }
+    </style>
+
+    <?php include "register.php";?>
+
+    <form action="" method="post">
+
+        <div class="table-container">
+            <h2>Lista de manageri si Curieri</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Role</th>
+                        <th>Nume</th>
+                        <th>Prenume</th>
+                        <th>Nr. telefon</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody id="managesAndCouriersInfoTable">
+                </tbody>
+            </table>
+        </div>
+
+    </form>
+
+</body>
+
 </html>
