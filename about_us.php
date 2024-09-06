a89f990feea57febe53143d836639b8b7b250854
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-09-06 19:12:26 +0300

    about us / contacts pages

--- about_us.php
+++ about_us.php
@@ -5,10 +5,10 @@
 <html lang="en">
 
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chișinău Dream Cars - About Us</title>
+    <title>Chișinău Dream Cars - Despre Noi</title>
+
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" type="image/x-icon" href="/images/logo.svg">
 
@@ -16,25 +16,34 @@
 </head>
 
 <body>
+    <style>
+    body {
+        justify-content: space-between;
+    }
+    </style>
+    <?php include('./reusable/header.php'); ?>
 
-    <?php include('./reusable/header.php');
-        
-        $currentPage = $_SERVER['SCRIPT_NAME'];
-        
-        if ($currentPage != "/index.php"){
-            echo '
-            <style>
-            .header-area{
-                margin-bottom:100px;
-            }
-            .header__bottom {
-                display: none;
-            }
-            </style>';
-        }
-    ?>
+    <section class="about_us_container">
+        <h1 class="about_us_title">Despre noi</h1>
 
-    <h1 style="margin:450px 0; color:white; font-size:60px">404</h1>
+        <p>Suntem o agenție de Închirieri Auto în R.Moldova, care are drept scop prestarea serviciilor de nișă, cu o experiență de primă clasă.</p>
+        <span>Viziunea companiei</span>
+
+        <p>Viziunea noastră este de a deveni cel mai important operator în domeniul Închirieri Auto din Republica Moldova, prin dezvoltarea continuă de noi relații și oportunități, și întotdeauna oferind servicii de înaltă calitate . </p>
+
+        <span>Valorile companiei</span>
+
+        <p><b>Respect:</b> Colaborăm împreună cu partenerii și furnizorii noștri cu respect și integritate pentru a dezvolta și menține relații de muncă de lungă durată și reciproc avantajoase.</p>
+
+        <p><b>Pasiunea:</b> Suntem pasionați de afacerea noastră. Avem grijă de clienții noștri prin calitatea serviciilor noastre; promovăm ideile și soluțiile creative și inovatoare.</p>
+
+        <p><b>Excelență:</b> Ne-am angajat să fim o organizație de înaltă performanță, focusată pe satisfacția totală a clientului. Analizăm continuu procesele noastre și pe noi înșine pentru a deveni cei mai buni dintre cei mai buni.</p>
+
+
+    </section>
 
     <?php include('./reusable/footer.php'); ?>
+
 </body>
+
+</html
--- contacts.php
+++ contacts.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chișinău Dream Cars - contacts</title>
+    <title>Chișinău Dream Cars - Contacte</title>
 
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" type="image/x-icon" href="/images/logo.svg">
@@ -16,24 +16,51 @@
 </head>
 
 <body>
-    <?php include('./reusable/header.php');
-        $currentPage = $_SERVER['SCRIPT_NAME'];
-        
-        if ($currentPage != "/index.php"){
-            echo '
-            <style>
-            .header-area{
-                margin-bottom:100px;
-            }
-            .header__bottom {
-                display: none;
-            }
+    <style>
+    body {
+        justify-content: space-between;
+    }
+    </style>
+    <?php include('./reusable/header.php'); ?>
 
-            </style>';
-        }
-    ?>
+    <section class="contacts_container">
+        <h1 class="contacts_title">Contacte</h1>
 
-    <h1 style="margin:450px 0; color:white; font-size:60px">404</h1>
+        <div class="contacts_card_container">
+            <div class="contacts_card">
+                <div class="contacts_card_title">Număr de telefon</div>
+                <a href="tel:+373 00 000 000">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.63913 0.549061L5.63934 0.549003C6.28262 0.373262 6.95311 0.683345 7.23722 1.27772L7.26781 1.35113L9.14272 5.85093L9.14281 5.85113C9.37636 6.41098 9.21523 7.05655 8.74407 7.44173L6.43325 9.3307L6.13053 9.57816L6.29772 9.93161C7.90804 13.336 10.664 16.092 14.0684 17.7023L14.4214 17.8693L14.6689 17.5672L16.5626 15.2564L16.5643 15.2542C16.9437 14.786 17.5925 14.6231 18.1536 14.8572L18.1538 14.8573L22.6536 16.7322L22.6546 16.7326C23.2929 16.9968 23.6333 17.6934 23.451 18.3607L23.4509 18.3609L22.3264 22.4843C22.3263 22.4845 22.3263 22.4847 22.3262 22.4849C22.1604 23.0849 21.616 23.5 20.9991 23.5C9.67885 23.5 0.5 14.3211 0.5 3.00092C0.5 2.38407 0.915015 1.83965 1.51506 1.67381C1.51528 1.67375 1.5155 1.67369 1.51572 1.67363L5.63913 0.549061Z" stroke="white" stroke-opacity="0.8" />
+                    </svg>
+                    +373 00 000 000
+                </a>
+            </div>
+
+            <div class="contacts_card">
+                <div class="contacts_card_title">E-mail</div>
+                <a href="mailto:">Loremipumdolor@gmail.com</a>
+            </div>
+
+            <div class="contacts_card">
+                <div class="contacts_card_title">Adresa în Chișinău</div>
+                <a href="https://maps.app.goo.gl/sJXQF2b6v2kJZpLy6">Chișinău, Lorem ipsum dolor</a>
+            </div>
+
+            <div class="contacts_card">
+                <div class="contacts_card_title">Adresa în Bălți</div>
+                <a href="https://maps.app.goo.gl/sJXQF2b6v2kJZpLy6">Bălți, Lorem ipsum dolor, 13A</a>
+            </div>
+        </div>
+
+        <div class="contacts_map">
+            <div class="contacts_map_buttons">
+                <div class="contacts_map_ch">Adresa în Chișinău</div>
+                <div class="contacts_map_ch">Adresa în Bălți</div>
+            </div>
+            <iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52764.087305644236!2d28.82340750833593!3d47.01690483484894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c3470a2d9b5%3A0xc74f4ac8ebc6b645!2z0KLRgNC40YPQvNGE0LDQu9GM0L3QsNGPINCQ0YDQutCw!5e0!3m2!1sru!2s!4v1725637372301!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
 
     <?php include('./reusable/footer.php'); ?>
 
